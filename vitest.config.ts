3c3e203bbdc4c31c4fe73fff436400db6edc5249
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-04-27 13:25:09 +0300

    fix: remove some configration of yaml

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export default defineConfig({
   test: {
-    globals: true,
+    globals: true, 
     environment: "node",
     alias: {
       // This tells Vitest: "When you see @prisma/client, look here instead"
