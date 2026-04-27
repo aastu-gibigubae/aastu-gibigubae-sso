3ca11dbccefbdd951fb86203c10d34d837e67cb8
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-04-27 13:34:29 +0300

    fix: move generated files to node modules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "vitest/config";
-import path from "path";
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
-    globals: true, 
-    environment: "node",
+    globals: true,
+    environment: 'node',
     alias: {
-      // This tells Vitest: "When you see @prisma/client, look here instead"
-      "@prisma/client": path.resolve(__dirname, "./src/generated/prisma"),
+      '@prisma/client': path.resolve(__dirname, './node_modules/@prisma/client')
     },
   },
 });
